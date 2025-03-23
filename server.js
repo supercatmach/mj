66e0b1a30f8995d18ea0102ef8ef6b04ad03bae1
Hulu <hulucat1024@gmail.com>
2025-03-23 20:51:42 +0800

    添加 CSP 設定，允許來自 Render 的連接

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const helmet = require('helmet');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+const io = socketIo(server);
+
+// 使用 Helmet 來設置 CSP
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"], // 預設來源
+            connectSrc: ["'self'", 'https://mj-5x4w.onrender.com'] // 允許從 Render 上的伺服器連接
+        }
     }
+}));
+
+// 其他伺服器設定
+app.get("/", (req, res) => {
+    res.send("麻將遊戲伺服器運行中");
 });
 
-// 當有玩家連線時
-io.on("connection", (socket) => {
-    console.log(`玩家已連線: ${socket.id}`);
-
-    // 監聽「message」事件
-    socket.on("message", (msg) => {
-        console.log(`收到訊息: ${msg}`);
-        io.emit("message", msg); // 把訊息發送給所有人
-    });
-
-    // 當玩家斷線時
-    socket.on("disconnect", () => {
-        console.log(`玩家已斷線: ${socket.id}`);
-    });
-});
-
-// 設定伺服器的監聽埠
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-    console.log(`伺服器運行中： http://localhost:${PORT}`);
+    console.log(`伺服器運行在 http://localhost:${PORT}`);
 });
