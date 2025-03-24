8c1b1082e807e998f2a842308af8ec5662e0d796
Hulu <hulucat1024@gmail.com>
2025-03-24 22:06:58 +0800

    ?湔

--- server.js
+++ server.js
@@ -19,8 +19,32 @@ allplayer=[]
 io.on("connection", (socket) => {
     console.log(`玩家已連線: ${socket.id}`);
 
+socket.on("move", (movnew) => {
+
+JSON.parse
+
+for(let i=0;i<allplayer.length;i++){
+
+if(allplayer[i].ids==socket.id){
+
+allplayer[i].inX=JSON.parse(movnew)[0]
+
+allplayer[i].inY=JSON.parse(movnew)[1]
+
+io.emit("move", JSON.stringify([socket.id,JSON.parse(movnew)[0],JSON.parse(movnew)[1]]));
+
+break
+
+}
+
+}
+
+});
+
+
+
     // 監聽訊息發送事件
-    socket.on("name", (pled) => {
+socket.on("name", (pled) => {
 
 for(let s=0;s<allplayer.length;s++){
 
@@ -28,11 +52,11 @@ io.to(socket.id).emit("pledlined",JSON.stringify(allplayer[s]));
 
 }
 
-        io.emit("pledonline", JSON.stringify([pled,socket.id]));
+io.emit("pledonline", JSON.stringify([pled,socket.id]));
 
 allplayer.push({"name":pled,"ids":socket.id,"inX":"250px","inY":"250px"})
 
-    });
+});
 
     // 監聽訊息發送事件
     socket.on("message", (msg) => {
