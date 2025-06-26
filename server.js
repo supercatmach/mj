50a9c31ce8569e9583c33eb48f231045de8d9016
Hulu <hulucat1024@gmail.com>
2025-06-27 02:41:00 +0800

    ?湔鈭?

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ function createRoomStructure(hostId) {
 }
 
 const rooms = {};
-///rooms["025024"] = { host: "貓貓", players: [] ,playerid: []  ,playerpic: [],ynstar:0,ynfriend:0,alps:0,epgh:[],pled:0,allmgd:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
+rooms["025024"] = { host: "貓貓", players: [] ,playerid: [] ,playerpic: [] ,ynstar:0,ynfriend:0,alps:0,epgh:[],pled:0,allmgd:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
 
 io.on("connection", (socket) => {
 
