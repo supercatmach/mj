ff7467090cc2a60da02f8262f6bd1683a1593c9a
Hulu <hulucat1024@gmail.com>
2025-07-04 02:09:54 +0800

    ?湔鈭?

--- maxatk.js
+++ maxatk.js
@@ -217,7 +217,7 @@ return
 
 if(ephchick==1&&ple!=0&&epgtw!="gun"&&epgtw!="tin"){///如果有吃碰槓.但是被強制取消則返回
 
-socket.emit("needgetcard",JSON.stringify([roomId,pled]));
+///socket.emit("needgetcard",JSON.stringify([roomId,pled]));
 
 ephchick=0
 }
