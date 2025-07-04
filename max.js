f7ecb05c3bc2629651db0184a65b6159fda1e92b
Hulu <hulucat1024@gmail.com>
2025-07-04 19:03:31 +0800

    ?湔鈭?

--- max.js
+++ max.js
@@ -227,7 +227,7 @@ return
 
 if(ephchick==1&&ple!=0&&epgtw!="gun"&&epgtw!="tin"){///如果有吃碰槓.但是被強制取消則返回
 
-socket.emit("needgetcard",JSON.stringify([roomId,pled,"不吃碰"]));
+socket.emit("needgetcard",JSON.stringify([roomId,pled,card[1]]));
 
 ephchick=0
 
